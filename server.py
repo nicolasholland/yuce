d1f588b9e1b49b705709cc358ec70803e0a9e903
nicolas holland <dutchman@linux.fritz.box>
2020-01-11 13:21:32 +0100

    Added combined image is served by flask

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import combine
 
 server = Flask(__name__)
 
 @server.route('/')
 def serve():
     return render_template('app.html')
-    
+
+@server.route('/current.png')
+def current():
+    image = combine.image()
+    return Response(image.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
     server.run(port=5000, debug=True)

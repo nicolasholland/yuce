e286a0900b3156f44ce999e6a5d5aca1ebc323ff
nicolas holland <dutchman@linux.fritz.box>
2020-02-08 10:44:12 +0100

    Added time delay for sat data

--- himawari.py
+++ himawari.py
@@ -4,7 +4,7 @@ import requests
 import imageio
 
 def saturl():
-    time = (pd.Timestamp.utcnow() - pd.Timedelta("10min")).floor("10min")
+    time = (pd.Timestamp.utcnow() - pd.Timedelta("15min")).floor("10min")
     base = "https://www.data.jma.go.jp/mscweb/data/himawari/img/fd_/"
     retval = base + time.strftime("fd__b03_%H%M.jpg")
     return retval

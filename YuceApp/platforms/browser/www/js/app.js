eb802b8cda50f8721c5d2b19b0a37f424aab413a
nicolas holland <dutchman@linux.fritz.box>
2020-07-14 22:29:10 +0200

    enh: load json from heroku (does work on browser but not on mobile...

--- YuceApp/platforms/browser/www/js/app.js
+++ YuceApp/platforms/browser/www/js/app.js
@@ -1,8 +1,43 @@
-localStorage["url"] = 'http://localhost:4444/data2.json';
-localStorage["api"] = 'http://localhost:4444/token.json';
-localStorage["auth"] = '{"auth": "key1"}';
+google.charts.load('current', {'packages':['line', 'corechart']});
+google.charts.setOnLoadCallback(drawLineChart);
 
-$.post(localStorage["api"], localStorage["auth"], function (jsonresponse) {
-    localStorage["token"] = JSON.stringify(jsonresponse);
-}, "json");
 
+function getData () {
+    $.get("http://wodeyuce.herokuapp.com/data.json", function (jsonresponse) {
+        localStorage["data"] = JSON.stringify(jsonresponse);
+    }, "json");
+
+    var data = new google.visualization.DataTable(localStorage["data"]);
+    return data;
+}
+
+function state1() {
+    getData();
+    drawLineChart();
+}
+
+function drawLineChart() {
+    var data = getData();
+
+    var options = {
+        title: '太阳光预测',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        series: {
+            0: {targetAxisIndex: 0}
+        },
+        vAxes: {
+            0: {title: 'Ghi (W/m²)'}
+        },
+        vAxis: {
+            viewWindow: {
+                min: 0,
+                max: 1000
+            }
+        }
+    };
+
+    var chart = new google.visualization.LineChart(
+        document.getElementById('lineplot'));
+        chart.draw(data, options);
+}

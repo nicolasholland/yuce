18f07e3c8be9169de0e3fc1a7e9e0a36f4b6b034
nicolas holland <dutchman@linux.fritz.box>
2020-07-20 23:45:36 +0200

    Added states

--- YuceApp/www/js/app.js
+++ YuceApp/www/js/app.js
@@ -1,5 +1,7 @@
 google.charts.load('current', {'packages':['line', 'corechart']});
-google.charts.setOnLoadCallback(drawLineChart);
+google.charts.setOnLoadCallback(gfs);
+
+var state = [ 0 ];
 
 
 function getData () {
@@ -24,16 +26,41 @@ function nwp () {
     if (chart.style.display == "none") {
         chart.style.display = "block";
     }
+    state = [0, 1, 2, 3, 4];
+    var data = new google.visualization.DataTable(
+        localStorage["data"]);
+    var view = new google.visualization.DataView(data);
+    view.setColumns(state);
+    drawLineChart(view);
+}
+
+function gfs () {
+    var data = getData();
+    drawLineChart(data);
+}
+
+function stateColumns(val) {
+    if (state.includes(val)) {
+        const index = state.indexOf(val);
+        if (index > -1) {
+            state.splice(index, val);
+        }
+    } else {
+        state.push( val )
+    }
 }
 
 function changeState(val) {
-    getData();
-    drawLineChart();
+    var data = new google.visualization.DataTable(
+        localStorage["data"]);
+
+    var view = new google.visualization.DataView(data);
+    stateColumns(val);
+    view.setColumns(state);
+    drawLineChart(view);
 }
 
-function drawLineChart() {
-    var data = getData();
-
+function drawLineChart(data) {
     var options = {
         title: '太阳光预测',
         curveType: 'function',
